3401a7c58397f1ca02eca7e9d8ce3f1f38902e90
unknown <hoang.vls.60cntt@ntu.edu.vn>
2021-01-20 01:25:06 +0700

    first commit

---
+++ ailatrieuphu_1/fcapnhap.Designer.cs
@@ -0,0 +1,284 @@
+﻿
+namespace ailatrieuphu_1
+{
+    partial class fcapnhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnthem = new System.Windows.Forms.Button();
+            this.btnxoa = new System.Windows.Forms.Button();
+            this.btnsua = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tba = new System.Windows.Forms.TextBox();
+            this.tbcn = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnok = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbch = new System.Windows.Forms.TextBox();
+            this.tbda = new System.Windows.Forms.TextBox();
+            this.tbd = new System.Windows.Forms.TextBox();
+            this.tbc = new System.Windows.Forms.TextBox();
+            this.tbb = new System.Windows.Forms.TextBox();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnthem
+            // 
+            this.btnthem.Location = new System.Drawing.Point(522, 303);
+            this.btnthem.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnthem.Name = "btnthem";
+            this.btnthem.Size = new System.Drawing.Size(123, 46);
+            this.btnthem.TabIndex = 0;
+            this.btnthem.Text = "Thêm";
+            this.btnthem.UseVisualStyleBackColor = true;
+            this.btnthem.Click += new System.EventHandler(this.btnthem_Click);
+            // 
+            // btnxoa
+            // 
+            this.btnxoa.Location = new System.Drawing.Point(26, 303);
+            this.btnxoa.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnxoa.Name = "btnxoa";
+            this.btnxoa.Size = new System.Drawing.Size(105, 46);
+            this.btnxoa.TabIndex = 1;
+            this.btnxoa.Text = "Xóa";
+            this.btnxoa.UseVisualStyleBackColor = true;
+            this.btnxoa.Click += new System.EventHandler(this.btnxoa_Click);
+            // 
+            // btnsua
+            // 
+            this.btnsua.Location = new System.Drawing.Point(271, 303);
+            this.btnsua.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnsua.Name = "btnsua";
+            this.btnsua.Size = new System.Drawing.Size(122, 46);
+            this.btnsua.TabIndex = 2;
+            this.btnsua.Text = "Sửa";
+            this.btnsua.UseVisualStyleBackColor = true;
+            this.btnsua.Click += new System.EventHandler(this.btnsua_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 245);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Đáp án:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(21, 213);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(22, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "D:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 173);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "C:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "B:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "A:";
+            // 
+            // tba
+            // 
+            this.tba.Location = new System.Drawing.Point(85, 89);
+            this.tba.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tba.Name = "tba";
+            this.tba.Size = new System.Drawing.Size(500, 22);
+            this.tba.TabIndex = 4;
+            // 
+            // tbcn
+            // 
+            this.tbcn.Location = new System.Drawing.Point(189, 4);
+            this.tbcn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbcn.Name = "tbcn";
+            this.tbcn.Size = new System.Drawing.Size(393, 22);
+            this.tbcn.TabIndex = 1;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(23, 7);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(160, 17);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Chọn dòng để cập nhật:";
+            // 
+            // btnok
+            // 
+            this.btnok.Location = new System.Drawing.Point(588, 4);
+            this.btnok.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnok.Name = "btnok";
+            this.btnok.Size = new System.Drawing.Size(57, 22);
+            this.btnok.TabIndex = 3;
+            this.btnok.Text = "OK";
+            this.btnok.UseVisualStyleBackColor = true;
+            this.btnok.Click += new System.EventHandler(this.btnok_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.tbch);
+            this.panel2.Controls.Add(this.tbda);
+            this.panel2.Controls.Add(this.tbd);
+            this.panel2.Controls.Add(this.tbc);
+            this.panel2.Controls.Add(this.tbb);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.tba);
+            this.panel2.Location = new System.Drawing.Point(26, 33);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(619, 266);
+            this.panel2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 17);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Câu Hỏi :";
+            // 
+            // tbch
+            // 
+            this.tbch.Location = new System.Drawing.Point(85, 26);
+            this.tbch.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbch.Multiline = true;
+            this.tbch.Name = "tbch";
+            this.tbch.Size = new System.Drawing.Size(500, 49);
+            this.tbch.TabIndex = 16;
+            // 
+            // tbda
+            // 
+            this.tbda.Location = new System.Drawing.Point(85, 242);
+            this.tbda.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbda.Name = "tbda";
+            this.tbda.Size = new System.Drawing.Size(500, 22);
+            this.tbda.TabIndex = 15;
+            // 
+            // tbd
+            // 
+            this.tbd.Location = new System.Drawing.Point(85, 208);
+            this.tbd.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbd.Name = "tbd";
+            this.tbd.Size = new System.Drawing.Size(500, 22);
+            this.tbd.TabIndex = 14;
+            // 
+            // tbc
+            // 
+            this.tbc.Location = new System.Drawing.Point(85, 173);
+            this.tbc.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbc.Name = "tbc";
+            this.tbc.Size = new System.Drawing.Size(500, 22);
+            this.tbc.TabIndex = 13;
+            // 
+            // tbb
+            // 
+            this.tbb.Location = new System.Drawing.Point(85, 133);
+            this.tbb.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbb.Name = "tbb";
+            this.tbb.Size = new System.Drawing.Size(500, 22);
+            this.tbb.TabIndex = 12;
+            // 
+            // fcapnhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 360);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.btnok);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnsua);
+            this.Controls.Add(this.btnxoa);
+            this.Controls.Add(this.tbcn);
+            this.Controls.Add(this.btnthem);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "fcapnhap";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "fcapnhap";
+            this.Load += new System.EventHandler(this.fcapnhap_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnthem;
+        private System.Windows.Forms.Button btnxoa;
+        private System.Windows.Forms.Button btnsua;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tba;
+        private System.Windows.Forms.TextBox tbcn;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnok;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox tbda;
+        private System.Windows.Forms.TextBox tbd;
+        private System.Windows.Forms.TextBox tbc;
+        private System.Windows.Forms.TextBox tbb;
+        private System.Windows.Forms.TextBox tbch;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ ailatrieuphu_1/fcapnhap.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ailatrieuphu_1
+{
+    public partial class fcapnhap : Form
+    {
+        public fcapnhap()
+        {
+            InitializeComponent();
+        }
+
+        private void fcapnhap_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void capnhat()
+        {
+            Excel.Application xlApp;
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+            Excel.Range range;
+            xlApp = new Excel.Application();
+            string url = Application.StartupPath + "\\cau-hoi-1.xlsx";
+            xlWorkBook = xlApp.Workbooks.Open(url, 0, false, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(2);
+            range = xlWorkSheet.UsedRange;
+            string index = tbcn.Text;
+            Excel.Range cauhoi = (Excel.Range)xlWorkSheet.Cells[index, 1];
+            cauhoi.Value2 = tbch.Text;
+            Excel.Range a = (Excel.Range)xlWorkSheet.Cells[index, 2];
+            a.Value2 = tba.Text;
+            Excel.Range b = (Excel.Range)xlWorkSheet.Cells[index, 3];
+            b.Value2 = tbb.Text;
+            Excel.Range c = (Excel.Range)xlWorkSheet.Cells[index, 4];
+            c.Value2 = tbc.Text;
+            Excel.Range d = (Excel.Range)xlWorkSheet.Cells[index, 5];
+            d.Value2 = tbd.Text;
+            Excel.Range da = (Excel.Range)xlWorkSheet.Cells[index, 6];
+            da.Value2 = tbda.Text;
+            xlWorkBook.SaveAs(url, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                Excel.XlSaveAsAccessMode.xlNoChange,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+            xlWorkBook.Close(true, null, null);
+
+            xlApp.Quit();
+
+            Marshal.ReleaseComObject(xlWorkSheet);
+            Marshal.ReleaseComObject(xlWorkBook);
+            Marshal.ReleaseComObject(xlApp);
+        }
+
+        private void btnthem_Click(object sender, EventArgs e)
+        {
+            Excel.Application xlApp;
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+            Excel.Range range;
+            int rw = 0; 
+            int cl = 0;
+            xlApp = new Excel.Application();
+            string url = Application.StartupPath + "\\cau-hoi-1.xlsx";
+            xlWorkBook = xlApp.Workbooks.Open(url, 0, false, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(2);
+            range = xlWorkSheet.UsedRange;
+            rw = range.Rows.Count;     
+            cl = range.Columns.Count;
+            // string index = tbcn.Text;
+           int index = rw+1;
+            Excel.Range cauhoi = (Excel.Range)xlWorkSheet.Cells[index, 1];
+            cauhoi.Value2 = tbch.Text;
+            Excel.Range a = (Excel.Range)xlWorkSheet.Cells[index, 2];
+            a.Value2 = tba.Text;
+            Excel.Range b = (Excel.Range)xlWorkSheet.Cells[index, 3];
+            b.Value2 = tbb.Text;
+            Excel.Range c = (Excel.Range)xlWorkSheet.Cells[index, 4];
+            c.Value2 = tbc.Text;
+            Excel.Range d = (Excel.Range)xlWorkSheet.Cells[index, 5];
+            d.Value2 = tbd.Text;
+            Excel.Range da = (Excel.Range)xlWorkSheet.Cells[index, 6];
+            da.Value2 = tbda.Text;
+            xlWorkBook.SaveAs(url, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                Excel.XlSaveAsAccessMode.xlNoChange,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+            xlWorkBook.Close(true, null, null);
+
+            xlApp.Quit();
+
+            Marshal.ReleaseComObject(xlWorkSheet);
+            Marshal.ReleaseComObject(xlWorkBook);
+            Marshal.ReleaseComObject(xlApp);
+
+            MessageBox.Show("Thêm thành công!!");
+        }
+      
+        private void laycauhoi()
+        {
+            Excel.Application xlApp;
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+            Excel.Range range;
+
+
+            int rw = 0; //số hàng tối đa có trong sheet
+            int cl = 0; //số cột tối đa có trong sheet
+
+            xlApp = new Excel.Application();
+            string url = Application.StartupPath + "\\cau-hoi-1.xlsx";
+            xlWorkBook = xlApp.Workbooks.Open(url, 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(2);//Truyền số vào đây để đọc sheet (cấp độ của câu 
+            range = xlWorkSheet.UsedRange;
+            rw = range.Rows.Count;      //Đếm số hàng
+            cl = range.Columns.Count;   //Đếm số cột 
+            string index= tbcn.Text;
+
+            tbch.Text = (string)(range.Cells[index, 1] as Excel.Range).Value2;
+            tba.Text = (string)(range.Cells[index, 2] as Excel.Range).Value2;
+            tbb.Text = (string)(range.Cells[index, 3] as Excel.Range).Value2;
+            tbc.Text = (string)(range.Cells[index, 4] as Excel.Range).Value2;
+            tbd.Text = (string)(range.Cells[index, 5] as Excel.Range).Value2;
+            tbda.Text = (string)(range.Cells[index, 6] as Excel.Range).Value2;
+
+            xlWorkBook.Close(true, null, null);
+            xlApp.Quit();
+
+            Marshal.ReleaseComObject(xlWorkSheet);
+            Marshal.ReleaseComObject(xlWorkBook);
+            Marshal.ReleaseComObject(xlApp);
+
+
+        }
+        private void loadCh(int ch)
+        {
+          
+        }
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void btnok_Click(object sender, EventArgs e)
+        {
+            laycauhoi();
+        }
+
+        private void btnsua_Click(object sender, EventArgs e)
+        {
+            capnhat();
+            MessageBox.Show("Sữa thành công!!");
+        }
+
+        private void btnxoa_Click(object sender, EventArgs e)
+        {
+            Excel.Application xlApp;
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+            Excel.Range range;
+
+
+            int rw = 0; //số hàng tối đa có trong sheet
+            int cl = 0; //số cột tối đa có trong sheet
+            xlApp = new Excel.Application();
+            string url = Application.StartupPath + "\\cau-hoi-1.xlsx";
+            xlWorkBook = xlApp.Workbooks.Open(url, 0, false, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(2);//Truyền số vào đây để đọc sheet (cấp độ của câu 
+            range = xlWorkSheet.UsedRange;
+            rw = range.Rows.Count;      //Đếm số hàng
+            cl = range.Columns.Count;   //Đếm số cột 
+            string index = tbcn.Text;
+
+            tbch.Text = (string)(range.Cells[index, 1] as Excel.Range).Value2;
+            tba.Text = (string)(range.Cells[index, 2] as Excel.Range).Value2;
+            tbb.Text = (string)(range.Cells[index, 3] as Excel.Range).Value2;
+            tbc.Text = (string)(range.Cells[index, 4] as Excel.Range).Value2;
+            tbd.Text = (string)(range.Cells[index, 5] as Excel.Range).Value2;
+            tbda.Text = (string)(range.Cells[index, 6] as Excel.Range).Value2;
+            xlWorkSheet.Rows[index].Delete();
+            xlWorkBook.SaveAs(url, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+               Excel.XlSaveAsAccessMode.xlNoChange,
+               Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+            xlWorkBook.Close(true);
+            xlApp.Quit();
+
+            Marshal.ReleaseComObject(xlWorkSheet);
+            Marshal.ReleaseComObject(xlWorkBook);
+            Marshal.ReleaseComObject(xlApp);
+
+            MessageBox.Show("Xoá thành công!!");
+        }
+    }
+
+}
